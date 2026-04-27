4844a5d4adaa6c5f42e6df366f97de8fa18598f1
svewap <wappler@wappler.systems>
2026-04-27 15:45:57 +0200

    [BUGFIX] VariablesViewHelper::renderChildren(): mixed return type

--- Classes/ViewHelpers/Misc/VariablesViewHelper.php
+++ Classes/ViewHelpers/Misc/VariablesViewHelper.php
@@ -77,10 +77,10 @@ class VariablesViewHelper extends AbstractViewHelper
      *
      * @return mixed The finally rendered child nodes.
      */
-    public function renderChildren()
+    public function renderChildren(): mixed
     {
         $content = parent::renderChildren();
-        return str_replace('-&gt;', '->', $content);
+        return str_replace('-&gt;', '->', (string)$content);
     }
 
     /**
